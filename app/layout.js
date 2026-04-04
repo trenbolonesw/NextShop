93133001273df63268e320e0b92a8dfb2751b712
John Doe <johndoe@email.com>
2026-04-04 18:53:41 -0500

    added og image

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,14 @@ import {home} from './page.module.css'
 export const metadata = {
   title:'NextShop',
   description:'Your new favorite online ecommerce site',
-
+ images:[
+  {
+    url:'/opengraph-image.png',
+     width: 1200,
+        height: 630,
+        alt: 'NextShop preview image',
+  }
+ ]
 }
 
 
