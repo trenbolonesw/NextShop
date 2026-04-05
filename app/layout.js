4fa3ec04aab552a2ee8b09006e67adf2cf8c2098
John Doe <johndoe@email.com>
2026-04-04 20:44:12 -0500

    new og

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
   url:"https://next-shop-rouge-alpha.vercel.app",
  images:[
   {
-    url:'https://res.cloudinary.com/dciywsh4y/image/upload/v1775352831/opengraph-image.png_gcyig3.png',
+    url:'https://res.cloudinary.com/dciywsh4y/image/upload/v1775353408/Group_3_2_z2picd.png',
      width: 1200,
         height: 630,
         alt: 'NextShop preview image',
